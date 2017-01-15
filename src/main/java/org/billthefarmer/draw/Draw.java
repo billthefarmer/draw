581f064c1f421ab0679ff0ea8ca1f3b17557e993
Bill Farmer <williamjfarmer@yahoo.co.uk>
2017-01-15 14:23:31 +0000

    Add more icons

--- src/main/java/org/billthefarmer/draw/Draw.java
+++ src/main/java/org/billthefarmer/draw/Draw.java
@@ -15,7 +15,9 @@ import android.view.View;
 public class Draw extends View
 {
     public static final int STEP = 64;
-    public static final int ICON_WIDTH = 240;
+    public static final int XSTEP = 56;
+    public static final int YSTEP = 48;
+    public static final int ICON_WIDTH = 224;
 
     private int width;
     private int height;
@@ -68,25 +70,27 @@ public class Draw extends View
 				    Color.WHITE,
 				    Shader.TileMode.CLAMP);
  	blue = new LinearGradient(0, 0, 0, -Main.WIDTH,
-				    Color.BLUE,
-				    Color.WHITE,
-				    Shader.TileMode.CLAMP);
+				  Color.BLUE,
+				  Color.WHITE,
+				  Shader.TileMode.CLAMP);
  	cyan = new LinearGradient(0, 0, 0, -Main.WIDTH,
-				    Color.CYAN,
-				    Color.WHITE,
-				    Shader.TileMode.CLAMP);
+				  Color.CYAN,
+				  Color.WHITE,
+				  Shader.TileMode.CLAMP);
  	magenta = new LinearGradient(0, 0, 0, -Main.WIDTH,
-				    Color.MAGENTA,
-				    Color.WHITE,
-				    Shader.TileMode.CLAMP);
+				     Color.MAGENTA,
+				     Color.WHITE,
+				     Shader.TileMode.CLAMP);
  	olive = new LinearGradient(0, 0, 0, -Main.WIDTH,
-				    0xff6f6f00,
-				    Color.WHITE,
-				    Shader.TileMode.CLAMP);
+				   Color.rgb(111, 111, 0),
+				   // 0xff6f6f00,
+				   Color.WHITE,
+				   Shader.TileMode.CLAMP);
  	aqua = new LinearGradient(0, 0, 0, -Main.WIDTH,
-				    0xffbfffbf,
-				    Color.WHITE,
-				    Shader.TileMode.CLAMP);
+				  Color.rgb(191, 255, 191),
+				  // 0xffbfffbf,
+				  Color.WHITE,
+				  Shader.TileMode.CLAMP);
     }
 
     @Override
@@ -108,6 +112,110 @@ public class Draw extends View
     }
 
     protected void drawIcon(Canvas canvas)
+    {
+	paint.setStyle(Paint.Style.FILL);
+	paint.setAntiAlias(true);
+
+	paint.setShader(black);
+	canvas.drawRoundRect(rect, STEP, STEP, paint);
+	// canvas.clipRect(rect);
+
+	paint.setStyle(Paint.Style.STROKE);
+	paint.setStrokeCap(Paint.Cap.BUTT);
+	paint.setShader(darkgreen);
+	paint.setStrokeWidth(8);
+
+	for (int i = -(ICON_WIDTH - STEP); i <= ICON_WIDTH - STEP; i += STEP)
+	    canvas.drawLine(i, -ICON_WIDTH, i, ICON_WIDTH, paint);
+
+	for (int i = -(ICON_WIDTH - STEP); i <= ICON_WIDTH - STEP; i += STEP)
+	    canvas.drawLine(-ICON_WIDTH, i, ICON_WIDTH, i, paint);
+
+	path.rewind();
+
+	// Sine
+	path.moveTo(-ICON_WIDTH, 0);
+	for (int x = -ICON_WIDTH; x <= ICON_WIDTH; x++)
+	{
+	    float y = (float)
+		Math.sin(x * 1 * Math.PI / ICON_WIDTH) * YSTEP * 3;
+	    path.lineTo(x, y);
+	}
+
+	paint.setShader(green);
+	paint.setStrokeCap(Paint.Cap.ROUND);
+	canvas.drawPath(path, paint);
+    }
+
+    protected void drawSiggenIcon(Canvas canvas)
+    {
+	paint.setStyle(Paint.Style.FILL);
+	paint.setAntiAlias(true);
+
+	paint.setShader(black);
+	canvas.drawRoundRect(rect, STEP, STEP, paint);
+	// canvas.clipRect(rect);
+
+	paint.setStyle(Paint.Style.STROKE);
+	paint.setStrokeCap(Paint.Cap.BUTT);
+	paint.setShader(darkgreen);
+	paint.setStrokeWidth(8);
+
+	for (int i = -(ICON_WIDTH - STEP); i <= ICON_WIDTH - STEP; i += STEP)
+	    canvas.drawLine(i, -ICON_WIDTH, i, ICON_WIDTH, paint);
+
+	for (int i = -(ICON_WIDTH - STEP); i <= ICON_WIDTH - STEP; i += STEP)
+	    canvas.drawLine(-ICON_WIDTH, i, ICON_WIDTH, i, paint);
+
+	path.rewind();
+
+	// Sine
+	path.moveTo(-ICON_WIDTH, -ICON_WIDTH + (STEP * 3) / 2);
+	for (int x = -ICON_WIDTH; x <= ICON_WIDTH; x++)
+	{
+	    float y = -ICON_WIDTH + (STEP * 3) / 2 - (float)
+		Math.sin(x * 4 * Math.PI / ICON_WIDTH) * YSTEP;
+	    path.lineTo(x, y);
+	}
+
+	// Square
+	path.moveTo(-ICON_WIDTH, 0);
+	path.rLineTo(0, -YSTEP);
+	path.rLineTo(XSTEP, 0);
+	path.rLineTo(0, YSTEP * 2);
+	path.rLineTo(XSTEP, 0);
+	path.rLineTo(0, -YSTEP * 2);
+	path.rLineTo(XSTEP, 0);
+	path.rLineTo(0, YSTEP * 2);
+	path.rLineTo(XSTEP, 0);
+	path.rLineTo(0, -YSTEP * 2);
+	path.rLineTo(XSTEP, 0);
+	path.rLineTo(0, YSTEP * 2);
+	path.rLineTo(XSTEP, 0);
+	path.rLineTo(0, -YSTEP * 2);
+	path.rLineTo(XSTEP, 0);
+	path.rLineTo(0, YSTEP * 2);
+	path.rLineTo(XSTEP, 0);
+	path.rLineTo(0, -YSTEP);
+
+	// Sawtooth
+	path.moveTo(-ICON_WIDTH, ICON_WIDTH - (STEP * 3) / 2);
+	path.rLineTo(XSTEP, -YSTEP);
+	path.rLineTo(0, YSTEP * 2);
+	path.rLineTo(XSTEP * 2, -YSTEP * 2);
+	path.rLineTo(0, YSTEP * 2);
+	path.rLineTo(XSTEP * 2, -YSTEP * 2);
+	path.rLineTo(0, YSTEP * 2);
+	path.rLineTo(XSTEP * 2, -YSTEP * 2);
+	path.rLineTo(0, YSTEP * 2);
+	path.rLineTo(XSTEP, -YSTEP);
+
+	paint.setShader(green);
+	paint.setStrokeCap(Paint.Cap.ROUND);
+	canvas.drawPath(path, paint);
+    }
+
+    protected void drawStrobeIcon(Canvas canvas)
     {
 	paint.setStyle(Paint.Style.FILL);
 	paint.setAntiAlias(true);
