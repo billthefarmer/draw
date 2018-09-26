2c8bed6bc6d1a17f8bd648d1b3082c5161eb5312
TacoTheDank <skytkrsfan3895@gmail.com>
2018-09-26 19:34:03 -0400

    java
    Signed-off-by: TacoTheDank <SkytkRSfan3895@gmail.com>

--- src/main/java/org/billthefarmer/draw/Main.java
+++ src/main/java/org/billthefarmer/draw/Main.java
@@ -1,87 +1,79 @@
 package org.billthefarmer.draw;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import java.io.File;
 import java.io.FileOutputStream;
 
-public class Main extends Activity
-{
+public class Main extends Activity {
     public static final int WIDTH = 512;
 
     private Draw draw;
 
     // Called when the activity is first created.
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-	draw = (Draw)findViewById(R.id.draw);
+        draw = findViewById(R.id.draw);
     }
 
     // On create option menu
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-	// Inflate the menu; this adds items to the action bar if it
-	// is present.
-	getMenuInflater().inflate(R.menu.main, menu);
-	return true;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it
+        // is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     // On options item
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-	// Get id
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Get id
 
-	int id = item.getItemId();
-	switch (id)
-	{
-	    // Settings
+        int id = item.getItemId();
+        switch (id) {
+            // Settings
 
-	case R.id.action_save:
-	    saveIcon();
+            case R.id.action_save:
+                saveIcon();
 
-	    return true;
+                return true;
 
-	default:
-	    return false;
-	}
+            default:
+                return false;
+        }
     }
 
     // saveIcon
-    private void saveIcon()
-    {
-	Bitmap bitmap =
-	    Bitmap.createBitmap(WIDTH, WIDTH, Bitmap.Config.ARGB_8888);
-	Canvas canvas = new Canvas(bitmap);
+    private void saveIcon() {
+        Bitmap bitmap =
+                Bitmap.createBitmap(WIDTH, WIDTH, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
 
-	canvas.drawColor(Color.TRANSPARENT);
-	canvas.translate(WIDTH / 2, WIDTH / 2);
+        canvas.drawColor(Color.TRANSPARENT);
+        canvas.translate(WIDTH / 2, WIDTH / 2);
 
-	if (draw != null)
-	    draw.drawHistogramIcon(canvas);
+        if (draw != null)
+            draw.drawHistogramIcon(canvas);
 
-	try
-	{
-	    File file = new File(getExternalFilesDir(null), "icon.png");
-	    FileOutputStream stream = new FileOutputStream(file);
+        try {
+            File file = new File(getExternalFilesDir(null), "icon.png");
+            FileOutputStream stream = new FileOutputStream(file);
 
-	    bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream);
-	    stream.close();
-	}
-
-	catch(Exception e) {}
+            bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream);
+            stream.close();
+        } catch (Exception e) {
+        }
     }
 }
